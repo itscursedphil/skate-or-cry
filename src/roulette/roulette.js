d8b97881a289a8c8054aacf79f11ea02821db44f
Philip Plagge <sayhi@itscursedphil.com>
2017-06-29 23:13:29 +0200

    WIP daily roulette

---
+++ src/roulette/roulette.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Container, Row, Col } from 'reactstrap';
+
+const RoulettePage = ({}) =>
+  <Container>
+    <Row>
+      <Col />
+    </Row>
+  </Container>;
