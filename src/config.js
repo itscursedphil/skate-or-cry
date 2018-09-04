fc0829d62c4aa4a7b5afa68022392ec5d1439746
Philip Plagge <sayhi@itscursedphil.com>
2018-09-05 00:13:02 +0200

    Changed start and end time for testing

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ import moment from "moment";
 
 export default {
   startDate: moment("01-09-2018", "DD-MM-YYYY"),
-  endDate: moment("02-09-2018", "DD-MM-YYYY")
+  endDate: moment("03-09-2018", "DD-MM-YYYY")
 };
