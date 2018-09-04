347c511abc47746d07afd6fe1d476dc59c88fe6e
Philip Plagge <sayhi@itscursedphil.com>
2018-09-04 21:21:30 +0200

    Configured start and end date for 2018 trip

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-import moment from 'moment';
+import moment from "moment";
 
 export default {
-  startDate: moment('19-07-2017', 'DD-MM-YYYY'),
-  endDate: moment('24-07-2017', 'DD-MM-YYYY')
+  startDate: moment("05-09-2018", "DD-MM-YYYY"),
+  endDate: moment("10-09-2018", "DD-MM-YYYY")
 };
